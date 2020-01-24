c045dc218e37e2751cbeebb810804619a49afc41
Ahmed-Zitouni <ahmedz123645@gmail.com>
2020-01-24 00:26:42 -0500

    Fixes

--- src/context/Products.js
+++ src/context/Products.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 import Gear1 from '../Icons/Gear1.jpg'
 import Gear3 from '../Icons/Gear3.jpg'
-
-
-
 
 const Data =  [
     {
@@ -30,37 +26,4 @@ const Data =  [
         Price: 40,
         Img: Gear3
     }]
-=======
-import Gear1 from '../Icons/Gear1.jpg'
-import Gear3 from '../Icons/Gear3.jpg'
-
-
-
-
-const Data =  [
-    {
-        qty : 1,
-        Size: 'n/a',
-        Name: 'Piston Floor Pump', 
-        Color: 'Red',
-        Price: 30,
-        Img: Gear1
-    },
-    {
-        qty : 1,
-        Size: 'n/a',
-        Name: 'Wheel Rack 2x', 
-        Color: 'Metallic Black',
-        Price: 56,
-        Img: 'https://images.giant-bicycles.com/b_white,c_pad,h_650,q_80/athu5ixyqd3gbvve95pt/58044_1.jpg'
-    },
-    {
-        qty : 1,
-        Size: 'n/a',
-        Name: 'Giro Bike Helmet', 
-        Color: 'Black/Green',
-        Price: 40,
-        Img: Gear3
-    }]
->>>>>>> 7db92891b7ba11b42058ab4ffbbe09438be7267d
 export {Data}
